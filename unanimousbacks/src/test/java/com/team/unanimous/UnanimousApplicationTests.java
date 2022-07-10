b2d994e0cc47d9c7bd44f58153adb4c246b9dbbc
DKKwag <105155862+DKKwag@users.noreply.github.com>
2022-07-11 07:10:30 +0900

    커밋

---
+++ unanimousbacks/src/test/java/com/team/unanimous/UnanimousApplicationTests.java
@@ -0,0 +1,9 @@
+package com.team.unanimous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class UnanimousApplicationTests {
+
+}
